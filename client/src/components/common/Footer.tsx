15959edb406c390f88f41fc90ad2da129a02ce1b
OhSeungYoung <osy0056@khu.ac.kr>
2021-01-04 01:05:08 +0900

    feat: Add text to Footer

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { respondTo } from '../../styles/mixin';
 
 const FooterArea = styled.footer`
   height: 10vh;
+  display: flex;
+  /* flex-direction: column; */
+  justify-content: center;
+  align-items: center;
+  ${respondTo.desktop`
+    font-size: 1.2rem;
+    `}
+  p {
+    margin: 0 0.5rem;
+  }
 `;
 
 function Footer() {
   return (
     <FooterArea>
-      <div></div>
+      <p>제4회 정부혁신제안 끝장개발대회 최우수 수상작,</p>
+      <a href="https://github.com/sarah5398/mind-detector">GitHub</a>
     </FooterArea>
   );
 }
